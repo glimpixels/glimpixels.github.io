0bffe5ac931274e3d3c619adbba7cc7eecce54f1
glimp <131330336+glimpixels@users.noreply.github.com>
2023-06-04 07:49:40 +0000

    ya ebal (fix 18)

--- 404.js
+++ 404.js
@@ -3,14 +3,14 @@
 //    
 //  }, 1000)
 
-function uhoh() {
-    var executed = false;
-    return function() {
-        if (!executed) {
-            executed = true;
-                document.getElementById("404").play();
-                document.getElementByClassName("yes").style.display="none";
-                document.getElementById("no").style.display="block";
-        }
-    };
-};
+//function uhoh() {
+//    var executed = false;
+//    return function() {
+//        if (!executed) {
+//            executed = true;
+//                document.getElementById("404").play();
+//                document.getElementByClassName("yes").style.display="none";
+//                document.getElementById("no").style.display="block";
+//        }
+//    };
+//};
