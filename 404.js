f29675ecfa0a8b2405ab3697ea1b38d1471cfc15
glimp <131330336+glimpixels@users.noreply.github.com>
2023-06-04 07:01:04 +0000

    WHAT AM I DOING (fix 11)

--- 404.js
+++ 404.js
@@ -1,4 +1,21 @@
-setTimeout(function(){
-    document.getElementById("404").play();
-    
-  }, 1000)
+//setTimeout(function(){
+//    document.getElementById("404").play();
+//    
+//  }, 1000)
+
+function uhoh{
+    something();
+}
+
+var something = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            setTimeout(function(){
+                document.getElementById("404").play();
+                
+              }, 1000)
+        }
+    };
+})();
